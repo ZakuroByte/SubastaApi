df493a1c1e5ae1515405e664467e11cd1f5f9cc3
ZakuroByte <alejandro.mnz018@gmail.com>
2026-06-06 16:23:38 -0600

    ProgramCircularFix

--- Program.cs
+++ Program.cs
@@ -41,7 +41,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHostedService<SubastaHolandesaService>();
